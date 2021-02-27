2fbda82273fdaaab8616d5bb5d5e406cc5b017e0
athkishore <kishore.asok@gmail.com>
2021-02-27 14:54:53 +0530

    corrections in db

--- app/models.py
+++ app/models.py
@@ -24,7 +24,7 @@ class Post(db.Model):
 class Category(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(64), index=True, unique=True)
-  posts = db.relationship('Post', backref='category', lazy='dynamic')  
+  posts = db.relationship('Post', backref='categ', lazy='dynamic')  
   
   def __repr__(self):
     return '<Category {}>'.format(self.name)
