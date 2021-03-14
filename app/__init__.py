11b28af14846c5e2bfef6160628b71c61d7d6e38
athkishore <kishore.asok@gmail.com>
2021-03-14 21:33:30 +0530

    featured article cards

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
